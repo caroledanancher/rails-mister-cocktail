eda1ebf04834eafd8091612b15b2ec87d7fbc171
carole danancher <carole.danancher@edhec.com>
2016-05-12 11:18:21 +0200

    models done with validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cocktail.destroy_all
+cocktails_attributes = [
+  {name: "Mojito"},
+  {name: "Mint Julep"},
+  {name: "Bloody Mary"},
+  {name: "Pina Colada"},
+  {name: "Sex on the Beach"},
+  {name: "Whiskey Sour"}
+]
+
+cocktails_attributes.each { |params| Cocktail.create!(params) }
+
+Ingredient.destroy_all
+ingredients_attributes = [
+  {name: "lemon"},
+  {name: "ice"},
+  {name: "whisky"},
+  {name: "vodka"},
+  {name: "orange juice"},
+  {name: "mint"}
+]
+
+ingredients_attributes.each { |params| Ingredient.create!(params) }
